adad1cf2175152ee2873c74b2dc2f4e0632e00b6
Joakimto <joakimto@chalmers.se>
2024-03-04 22:52:18 +0100

    Maybe working version

--- amazed_lab/amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
+++ amazed_lab/amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
@@ -4,11 +4,8 @@ import amazed.maze.Maze;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.concurrent.*
 
 /**
  * <code>ForkJoinSolver</code> implements a solver for
@@ -18,20 +15,21 @@ import java.util.concurrent.*
  * Instances of <code>ForkJoinSolver</code> should be run by a
  * <code>ForkJoinPool</code> object.
  */
+public class ForkJoinSolver extends SequentialSolver {
 
+    private static boolean found = false;
+    private int player;
 
-public class ForkJoinSolver
-    extends SequentialSolver
-{
     /**
      * Creates a solver that searches in <code>maze</code> from the
      * start node to a goal.
      *
      * @param maze   the maze to be searched
      */
-    public ForkJoinSolver(Maze maze)
-    {
+    public ForkJoinSolver(Maze maze) {
         super(maze);
+        this.player = maze.newPlayer(start);
+        this.visited = new ConcurrentSkipListSet<>();
     }
 
     /**
@@ -45,8 +43,7 @@ public class ForkJoinSolver
      *                    <code>forkAfter &lt;= 0</code> the solver never
      *                    forks new tasks
      */
-    public ForkJoinSolver(Maze maze, int forkAfter)
-    {
+    public ForkJoinSolver(Maze maze, int forkAfter) {
         this(maze);
         this.forkAfter = forkAfter;
     }
@@ -63,15 +60,70 @@ public class ForkJoinSolver
      *           be found.
      */
     @Override
-    public List<Integer> compute()
-    {
-        int startNode = maze.start();
+    public List<Integer> compute() {
         return parallelSearch();
     }
 
-    //TODO
-    private List<Integer> parallelSearch()
-    {
+    private List<Integer> waitForOtherSolvers(List<ForkJoinSolver> otherSolvers) {
+        List<Integer> result = null;
+
+        for (ForkJoinSolver solver : otherSolvers) {
+            List<Integer> partialPath = solver.join();
+            if (partialPath != null) result = partialPath;
+        }
+        return result;
+    }
+    
+    private ForkJoinSolver(Maze maze, int player, int start, Set<Integer> visited) {
+        super(maze);
+        this.player = player;
+        this.start = start;
+        this.visited = visited;
+    }
+    
+    private List<Integer> parallelSearch() {
+        ArrayList<ForkJoinSolver> otherSolvers = new ArrayList<>();
+        frontier.push(start);
+
+        while (!frontier.isEmpty() && !found) {
+            int currentNode = frontier.pop();
+
+            if (!visited.add(currentNode)) {
+                continue;
+            }
+            maze.move(player, currentNode);
+
+            if (maze.hasGoal(currentNode)) {
+                found = true;
+                return pathFromTo(start, currentNode);
+            }
+            ArrayList<Integer> unvisitedNeighbors = new ArrayList<>();
+
+            for (Integer nb : maze.neighbors(currentNode)) {
+                if (!visited.contains(nb)) unvisitedNeighbors.add(nb);
+            }
+            for (int i = 0; i < unvisitedNeighbors.size(); i++) {
+                int nb = unvisitedNeighbors.get(i);
+                predecessor.put(nb, currentNode);
+
+                if (i == 0) {
+                    frontier.push(nb);
+                } else {
+                    int newPlayer = maze.newPlayer(nb);
+                    ForkJoinSolver solver = new ForkJoinSolver(maze, newPlayer, nb, visited);
+                    otherSolvers.add(solver);
+                    solver.fork();
+                }
+            }
+        }
+        List<Integer> pathToGoal = waitForOtherSolvers(otherSolvers);
+
+        if (pathToGoal != null) {
+            int mid = pathToGoal.remove(0);
+            List<Integer> pathFromStart = pathFromTo(start, mid);
+            pathFromStart.addAll(pathToGoal);
+            return pathFromStart;
+        }
         return null;
     }
 }
